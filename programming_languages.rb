12c086f9953d4132e4d02e60aeced82fb260f136
Mary Boyd <mary.boyd314@gmail.com>
2016-07-22 16:48:26 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+new_hash = {}
+languages.each do |style, lang_data|
+  lang_data.each do |lang, type|
+    if new_hash[lang]
+      new_hash[lang][:style] << style
+    else
+      new_hash[lang] = type
+     new_hash[lang][:style] = [style]
+  end
+
 end
+end
+new_hash
+end
+
+
+#we didn't technically build up the hash before the if statement? -
+# else statement is setting up the language hash
